2fc2f58f3a2e5864bffbface05fe1c7900431d8a
Amanda LeMoine <amanda1@mnlemoines.com>
2021-01-19 19:34:48 -0800

    dotenv installed and config.json updated to js file

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const { DB_HOST, DB_USER, DB_PASS } = process.env;
+
+module.exports = {
+  "development": {
+    "username": DB_USER,
+    "password": DB_PASS,
+    "database": "space_invaders_db",
+    "host": DB_HOST,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": DB_USER,
+    "password": DB_PASS,
+    "database": "database_test",
+    "host": DB_HOST,
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
+  }
+};
