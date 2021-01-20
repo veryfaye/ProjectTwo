e9336bfd8084dfd42825b6eda823c7fb7865c39a
Amanda LeMoine <amanda1@mnlemoines.com>
2021-01-19 21:08:41 -0800

    navigation to members changed to endGame

--- public/js/signup.js
+++ public/js/signup.js
@@ -48,7 +48,7 @@ $(document).ready(() => {
       lastName: lastName
     })
       .then(() => {
-        window.location.replace("/members");
+        window.location.replace("/endGame");
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
